f81462fdd3e8bb0dbbd99d571c47de64fd835f16
Bruno Alves <75707537+br-alves-reis@users.noreply.github.com>
2024-01-30 20:49:50 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include "library.h"
+#include "library.h" // referenciando o arquivo headeres que é implementada pelo arquivo library.c que contém a implementação das funções descritas.
 
 int main(){
 
-  float raio, altura, volume;
+  float raio, altura, volume, area;
 
   raio = 5.58;
   altura = 12.4;
@@ -11,5 +11,11 @@ int main(){
   volume = Volume_Cilindro(raio, altura);
 
   printf("o Volume eh: %f", volume);
+
+  area = Area_Circulo(raio);
+
+  printf("a area do circulo eh: %f", area);
+
+  
   
 }
