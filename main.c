8ce7023f3ea3bc8f33866097dac12b10e17ff63c
Bruno Alves <75707537+br-alves-reis@users.noreply.github.com>
2024-01-30 20:36:18 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,11 +3,13 @@
 
 int main(){
 
-  float raio, altura;
+  float raio, altura, volume;
 
   raio = 5.58;
   altura = 12.4;
 
-  
+  volume = Volume_Cilindro(raio, altura);
 
+  printf("o Volume eh: %f", volume);
+  
 }
